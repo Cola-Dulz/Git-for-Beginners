b06b4dccad7c0aa728d06f72adc001fb7ea2d2f3
Valdec1 <VKHOMEN@SOFTSERVEINC.COM>
2026-07-22 18:20:10 +0200

    added divide_nums and updated main

--- Utils/calculator.py
+++ Utils/calculator.py
@@ -1,1 +1,5 @@
-dsfd
+def add_nums(a,b):
+    return a+b
+
+def divide_nums(a,b):
+    return a/b
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("I love goats")
+from utils.calculator import add_nums, divide_nums
+
+if __name__== "__main__":
+    print(add_nums(1,3))
+    print(divide_nums(5,3))
